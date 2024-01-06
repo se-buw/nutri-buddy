fc4428727d5fa91bc3b7d43a74008e57eac48f35
timlaeser <tim.laeser@gmail.com>
2024-01-06 15:56:47 +0100

    add some tests

--- src/test/java/de/buw/se4de/AppTest.java
+++ src/test/java/de/buw/se4de/AppTest.java
@@ -5,37 +5,34 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     private App app;
 
-    @BeforeEach
-    void setUp() {
-        Platform.startup(() -> {
-        });
-    }
-    @AfterEach
-    void tearDown() {
-        // Shutdown the JavaFX toolkit after each test
-        Platform.exit();
-    }
-
     @Test
     void readAccountMissingFile() {
+        Platform.startup(() -> {
+        });
         app = new App();
-        String empty = "";
-        empty = app.readAccount(0, "weight", "missing");
-        assertFalse(empty.isEmpty());
+        String result = app.readAccount(0, "weight", "missing");
+        assertThrows(FileNotFoundException.class, () ->app.readAccount(0, "weight", "missing"));
+        assertEquals("Error, Account 0 with data weightnot found", result);
     }
-    /*
+
     @Test
     void readAccountEmptyFile() {
         app = new App();
-        String empty = "";
-        //empty = app.readAccount(0, "weight", "scr/test/resources/empty.csv");
-        assertTrue(empty.isEmpty());
+        String result = app.readAccount(0, "weight", "src/test/resources/empty.csv");
+        assertEquals("Error, Account 0 with data weightnot found", result);
     }
-
-     */
+    @Test
+    void readAccountTestTest() {
+        app = new App();
+        String result = app.readAccount(0, "height", "src/main/resources/accounts.csv");
+        assertEquals("130", result);
+        Platform.exit();
+    }
 }
